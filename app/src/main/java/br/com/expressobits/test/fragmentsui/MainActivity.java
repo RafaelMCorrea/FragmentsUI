85edbfc1ed61d339f23490ca53ccd6d1a29585a8
Rafael Correa <rafinha2108@gmail.com>
2015-05-26 17:59:59 -0300

    Fragments feitos de forma flexível.

--- app/src/main/java/br/com/expressobits/test/fragmentsui/MainActivity.java
+++ app/src/main/java/br/com/expressobits/test/fragmentsui/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.expressobits.test.fragmentsui;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,29 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Verifique se a atividade está usando a versão de layout com o fragment_container
+        // FrameLayout
+        if(findViewById(R.id.fragment_container)!=null){
+            // No entanto, se estamos a ser restaurado a partir de um estado anterior,
+            // então não precisa fazer nada e deve retornar ou então poderíamos acabar
+            // com fragmentos sobrepostos.
+            if(savedInstanceState !=null){
+                return;
+            }
+            // Criar um novo fragmento a ser colocado na disposição actividade
+            HeadLinesFragment firstFragment = new HeadLinesFragment();
+
+            // No caso de essa atividade foi iniciada com instruções especiais de uma
+            // Intenção, passar extras da intenção de o fragmento como argumentos
+            firstFragment.setArguments(getIntent().getExtras());
+            // Adicionar o fragmento para o 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container,firstFragment).commit();
+
+
+            //get().setDisplayShowTitleEnabled(true);
+        }
     }
 
     @Override
